9ef4a0fb259d5475fa8e52c2e3e10017874040cb
ManojModhale <149600316+ManojModhale@users.noreply.github.com>
2023-12-05 23:59:16 +0530

    Add files via upload

---
+++ Quiz.java
@@ -0,0 +1,41 @@
+package task3;
+
+import java.util.ArrayList;
+
+public class Quiz 
+{
+	ArrayList<Question> questions;
+    private int currentQuestionIndex;
+    private int userScore;
+
+    public Quiz(ArrayList<Question> questions) 
+    {
+        this.questions = questions;
+        this.currentQuestionIndex = 0;
+        this.userScore = 0;
+    }
+
+    public Question getCurrentQuestion() {
+        return questions.get(currentQuestionIndex);
+    }
+
+    public boolean hasNextQuestion() {
+        return currentQuestionIndex < questions.size();
+    }
+
+    public void nextQuestion() 
+    {
+        if (hasNextQuestion()) 
+        {
+            currentQuestionIndex++;
+        }
+    }
+
+    public int getUserScore() {
+        return userScore;
+    }
+
+    public void incrementUserScore() {
+        userScore++;
+    }
+}
